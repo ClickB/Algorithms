bc65bfa5bd5ed78cbc540119871c7e8358968029
Charles <2455034358@qq.com>
2020-02-05 23:46:21 +0800

    Add ArrayDemo HelloWorld StaticDemo.

---
+++ java_array/src/StaticDemo/StaticDemo.java
@@ -0,0 +1,30 @@
+package StaticDemo;
+
+public class StaticDemo {
+
+    public static int abs(int x)
+    {
+        if (x < 0) {
+            return -x;
+        }
+        else {
+            return x;
+        }
+    }
+
+    //计算直角三角形的斜边
+    public static double hypotenuse(double a, double b) {
+        return Math.sqrt(a*a + b*b); //java的Math包
+    }
+
+    public static void main(String[] args){
+        int n = -17;
+        double hyp;
+
+        n = abs(n);
+        System.out.println(n);
+
+        hyp = hypotenuse(3.0, 4.1);
+        System.out.println(hyp);
+    }
+}
---
+++ java_array/src/come/charles/array/ArrayDemo.java
@@ -0,0 +1,43 @@
+package come.charles.array;
+
+public class ArrayDemo {
+
+    public static void print_array_2(int x) {
+        int M = 2;
+        int N = 3;
+        double [] [] a = new double[M][N];
+
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                a[i][j] = i + j * 0.1;
+                System.out.println(a[i][j]);
+            }
+        }
+
+        System.out.printf("x is %X.\n", x);
+
+
+    }
+
+    public static void main(String[] args) {
+        int N;
+        N = 2;
+        int i;
+        double[] a;
+        a = new double[N];
+
+        for (i = 0; i < N/2; i++)
+            a[i] = 1.1;
+        for (i = N/2; i < N; i++)
+            a[i] = 2.2;
+
+        for (i = 0; i < N; i++) {
+            System.out.println(a[i]);
+        }
+
+        print_array_2(0xF1);
+    }
+
+}
